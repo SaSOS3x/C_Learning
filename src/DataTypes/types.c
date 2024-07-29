6e167b323cbe19d52407310c1f5e0b519047066b
SaSOS3x <sanekmrd789@gmail.com>
2024-07-29 19:13:45 +0300

    update types.c

--- src/DataTypes/types.c
+++ src/DataTypes/types.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <locale.h>
+#include <math.h>
+
+
 
 int main(){
 
-    // целочисленные переменные
+    start(0);
 
-    start(1);
+    // целочисленные переменные
 
     int a_numb1 = 38;
     signed int a_numb2 = 2;
@@ -13,9 +16,23 @@ int main(){
 
     printf("numb1 is %d\n", a_numb1);
     printf("numb2 is %d\n", a_numb2);
-    printf("numb3 is %d\n", a_numb3);
+    printf("numb3 is %d\n\n", a_numb3);
 
     suff_int(0);
+
+    binar_data(0);
+
+    float_data(0);
+
+    char_data(1);
+
+
+    // Функция typedef
+    typedef unsigned char BYTE; // Изменил название типа данных для обращения к нему
+
+    BYTE byte = 22;
+    printf("byte = %d", byte);
+
 
     return 0;
 }
@@ -24,7 +41,9 @@ int start(int s_o){
 
     if (s_o == 1){
 
-        char *locate = setlocale(LC_ALL, "");
+        setlocale(LC_ALL, "");
+
+        //char *locate = setlocale(LC_ALL, "");
 
         printf("Суффиксы целочисленных типов:\n\nДля каждого типа данных, применяется свой суффикс:\n\n");
         printf("int, long, short - d\n");
@@ -96,7 +115,62 @@ int suff_int(int suff_o){
         printf("l_numb2 is %ld\n", l_numb2);
 
         printf("sll_numb1 is %lld\n", sll_numb1);
-        printf("sll_numb2 is %lld\n", sll_numb2);
+        printf("sll_numb2 is %lld\n\n", sll_numb2);
+
+        return 0;
+    }else{
+        return -1;
+    }
+}
+
+int binar_data(int b_o){
+
+    if (b_o == 1){
+        short code1 = 0b1011;
+        short code2 = 013;
+        short code3 = 11;
+        short code4 = 0xB;
+
+        printf("code1: 0b1011 - 2: %d\n", code1);
+        printf("code2: 013 - 8:    %d\n", code2);
+        printf("code3: 11 - 10:    %d\n", code3);
+        printf("code4: 0xB - 16:   %d\n\n", code4);
+
+        return 0;
+    }else{
+        return -1;
+    }
+    
+    //printf("sizeof(s_numb1) is %d", sizeof(s_numb1));
+}
+
+int float_data(int f_o){
+
+    if (f_o == 1){
+
+        double f_numb1 = 3.14;
+        long double f_numb2 = 134.834;
+        float f_numb3 = 3.141592653f;
+
+        printf("f_numb1 is %f\n", f_numb1);
+        printf("f_numb2 is %Lf\n", f_numb2);    // обязательно нужно использовать флаг компиляции для вывода этого значения в консоль, -D__USE_MINGW_ANSI_STDIO=1
+        printf("f_numb3 is %f\n\n", f_numb3);
+
+        return 0;
+    }else{
+        return -1;
+    }
+}
+
+int char_data(int char_o){
+
+    if (char_o == 1){
+
+        char letter = 'A'; //Указан символ в кодировке ASCII
+        letter = 65; //Указан код символа в кодировке ASCII
+
+        printf("letter: %d - int\n", letter);
+        printf("letter: %c - char\n", letter);
 
         return 0;
     }else{
