1d0b5edb45cddd2837cea1df6a58900ba32093ea
SaSOS3x <sanekmrd789@gmail.com>
2024-08-01 02:33:02 +0300

    create math_f.c

---
+++ src/Math/math_f.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <math.h>
+
+double sum(double a, double b){
+    return a + b;
+}
+
+double raz(double a, double b){
+    return a - b;
+}
+
+double multiple(double a, double b){
+    return a * b;
+}
+
+double division(double a, double b){
+    if (b == 0){
+        printf("Error: Division by zero is not allowed.\n");
+        return NAN;
+    }else{
+        return a / b;
+    }
+}
+
+int mod(int a, int b){
+    if (b == 0){
+        printf("Error: Division by zero is not allowed.\n");
+        return NAN;
+    }else{
+        return a % b;
+    }
+}
+
+int div(int a, int b){
+    if (b == 0){
+        printf("Error: Division by zero is not allowed.\n");
+        return NAN;
+    }else{
+        return a / b;
+    }
+}
+
+int main(void){
+
+    float a = sum(10.4, 5);
+    float b = raz(10.4, 5);
+    float c = multiple(10.4, 5);
+
+    printf("sum(10.4, 5) -> a = %0.3f\n", a);
+    printf("raz(10.4, 5) -> b = %0.3f\n", b);
+    printf("multiple(10.4, 5) -> c = %0.3f\n", c);
+
+
+    float d = division(10.4, 5);
+    printf("division(10.4, 5) -> d = %0.3f\n", d);
+    
+
+    int e = mod(10, 6);
+    printf("mod(10, 7) -> e = %d\n", e);
+
+    int f = div(10,6);
+    printf("div(10, 7) -> f = %d\n", f);
+
+    return 0;
+}
